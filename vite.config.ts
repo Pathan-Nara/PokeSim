ad887dd2748fd2f777e99b247e9adbb4cc3eb102
Pathan-Nara <nathan.para@coda-student.school>
2025-12-17 03:51:19 +0100

    test github pages ++

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-  base: "/pokesim/",
+  base: "/",
   plugins: [
     react(),
     VitePWA({
