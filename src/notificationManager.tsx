05f461caac7a6b9c4e38fdc11ae49e2ea26a42d5
Para <nathan.para@coda-student.school>
2025-12-17 16:10:23 +0100

    tentative de fix des notif sur mobile...

--- src/notificationManager.tsx
+++ src/notificationManager.tsx
@@ -4,6 +4,17 @@ export async function requestPermission() {
 
 export function sendNotification(title: string, options?: NotificationOptions) {
     if (Notification.permission === "granted") {
+        try {
+            navigator.serviceWorker.getRegistration().then(function(reg) {
+                if (reg) {
+                    reg.showNotification(title, options);
+                } else {
+                    console.warn("No service worker registration found.");
+                }
+            });
+        } catch (error) {
+            console.error("Error showing notification:", error);
+        }
         new Notification(title, options);
     } else {
         console.warn("Notification permission not granted.");
